9c4a2ef4e6baca2b8d4148740a0ace63178bfadc
lawos98 <85324038+lawos98@users.noreply.github.com>
2021-06-15 17:30:05 +0200

    Update Merge_sort.py

--- sorting algorithms/Merge_sort.py
+++ sorting algorithms/Merge_sort.py
@@ -16,26 +16,26 @@
 
 
 def mergesort(T):
-  if len(T)>1:
-    left=mergesort(T[0:len(T)//2])
-    right=mergesort(T[len(T)//2:len(T)])
+	if len(T) > 1:
+		left = mergesort(T[0:len(T) // 2])
+		right = mergesort(T[len(T) // 2:len(T)])
 
-    l=r=i=0
-    while l<len(left) and r<len(right):
-      if left[l]<right[r]:
-        T[i]=left[l]
-        l+=1
-      else:
-        T[i]=right[r]
-        r+=1
-      i+=1
-    while l<len(left):
-        T[i]=left[l]
-        l+=1
-        i+=1
-    while r<len(right):
-        T[i]=right[r]
-        r+=1
-        i+=1
+		l = r = i = 0
+		while l < len(left) and r < len(right):
+			if left[l] < right[r]:
+				T[i] = left[l]
+				l += 1
+			else:
+				T[i] = right[r]
+				r += 1
+			i += 1
+		while l < len(left):
+			T[i] = left[l]
+			l += 1
+			i += 1
+		while r < len(right):
+			T[i] = right[r]
+			r += 1
+			i += 1
 
-  return T
+	return T
