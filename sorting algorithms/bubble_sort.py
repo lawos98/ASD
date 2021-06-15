df9c78c23ff80b4a3d5918910d8cd79a1f725114
lawos98 <85324038+lawos98@users.noreply.github.com>
2021-06-15 14:32:03 +0200

    Update bubble_sort.py

---
+++ sorting algorithms/bubble_sort.py
@@ -0,0 +1,22 @@
+
+"""
+/=========================================================================================\
+|Algorytm sortowania Babelkowego                                                          |
+|                                                                                         |
+|Polega na porównywaniu dwóch kolejnych elementów i zamianie ich kolejności               |
+|jeżeli zaburza ona porządek, w jakim się sortuje tablicę. Sortowanie kończy się,         |
+|gdy podczas kolejnego przejścia nie dokonano żadnej zmiany.                              |
+|                                                                                         |
+|Złożoność czasowa :O(n^2)           Złożoność Pamięciowa O(1)                            |
+|                                                                                         |
+\=========================================================================================/
+"""
+
+def Bubble_sort(Array):
+	Flag=1
+	while(Flag):
+		Flag=0
+		for index in range(1,len(Array)):
+			if Array[index]<Array[index-1]:
+				Array[index],Array[index-1]=Array[index-1],Array[index]
+				Flag=1
